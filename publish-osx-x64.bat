03f35c539cc624158b92f1a3f8172382698d56be
Alan Edwardes <alan@alanedwardes.com>
2019-03-30 18:08:16 +0000

    Adds convenience .bat files for publishing.

---
+++ publish-osx-x64.bat
@@ -0,0 +1,2 @@
+dotnet publish src/Estranged.Workshop/Estranged.Workshop.csproj -r osx-x64 -c Release -o %cd%/publish/osx-x64
+pause
---
+++ publish-win-x64.bat
@@ -0,0 +1,2 @@
+dotnet publish src/Estranged.Workshop/Estranged.Workshop.csproj -r win-x64 -c Release -o %cd%/publish/win-x64
+pause
